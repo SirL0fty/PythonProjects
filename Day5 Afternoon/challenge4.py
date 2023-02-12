2a698c00196a2d05de2cd056cb3d484f53fee1a5
SirL0fty <sirl0fty@gmail.com>
2023-02-12 20:08:38 +0000

    Updated all comments

--- Day5 Afternoon/challenge4.py
+++ Day5 Afternoon/challenge4.py
@@ -1,21 +1,25 @@
 #4) ask the user for a number. Tell that user whether the number is odd or even
 
-# myNumber = int(input("Please enter a number: "))
-#
-# if myNumber % 2 == 0:
-#     print("This number is Even")
-# else:
-#     print("This number is Odd")
-#
+#creates the variable setting it as a number
+myNumber = int(input("Please enter a number: "))
+
+# Check if the number is even or odd
+# % modulo checks if multiples given number fits exactly into value it is checked against
+# i.e. 10 % 3 would leave 1, as 3, 3, 3, 1 equals 10
+if myNumber % 2 == 0:
+    print("This number is Even")
+else:
+    print("This number is Odd")
+
 
 #andys code
 
-num = int(input("Please enter a number: "))
-#
-# if num % 2 == 0:
-#      print("This number is Even")
-#  else:
-#     print("This number is Odd")
+# num = int(input("Please enter a number: "))
+# #
+# # if num % 2 == 0:
+# #      print("This number is Even")
+# #  else:
+# #     print("This number is Odd")
 
-print("Number was even" if num % 2 == 0 else "number was odd")
+# print("Number was even" if num % 2 == 0 else "number was odd")
 
--- Day5 Afternoon/challenge5.py
+++ Day5 Afternoon/challenge5.py
@@ -1,13 +1,29 @@
 #5)ask the user to put in their name. tell the user how many letters there are in their name. added credit - tell them if any of the letters are the same
 
-name = input("Enter your name: ")
-print(f"The number of letters is {len(name)}")
+name = input("Please enter a name: ").lower()
 
-repeats = set()
+# Get the length of the name
+name_length = len(name)
 
+# Print the length of the name
+print("Your name is", name_length, "characters long.")
+
+# Initialize an empty list to store repeating characters
+repeats = []
+
+# Loop through each character in the name
 for i in name:
+    # Check if the character repeats in the name
     if name.count(i) > 1:
-        repeats.add(i)
-print("Repeated letters")
-for i in repeats:
-    print(i)
+        # If the character repeats, check if it is already in the repeats list
+        if i not in repeats:
+            # If not, add it to the repeats list
+            repeats.append(i)
+
+# Check if there are any repeating characters
+if len(repeats) > 0:
+    # If there are, print the repeating characters separated by commas
+    print("The following letters repeat:", ', '.join(repeats))
+else:
+    # If there are no repeating characters, print a message
+    print("No letters repeat.")
