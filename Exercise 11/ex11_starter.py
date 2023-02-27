31b0b8122cc862b8c476a95fe68c503f02b99040
Mike Crane <sirl0fty@gmail.com>
2023-02-27 10:26:44 +0000

    Update ex11_starter.py

--- Exercise 11/ex11_starter.py
+++ Exercise 11/ex11_starter.py
@@ -14,5 +14,5 @@ yell(Belgium)#runs the definition
 new_Belgium = Belgium.replace(",", ":")#creates a new variable and uses the replace function
 print(new_Belgium)#prints new fucntion
 
-ls = new_Belgium.split(":")#Spilts the new list at the comma
+ls = new_Belgium.split(":")#Spilts the new list at the semicolon 
 print(int(ls[1]) + int(ls[3]))#converts the string to a number and add's them together to make 11183818
